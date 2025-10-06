dbd14becab21dfecd249706be5d367832d926a23
Balwant Chouhan <balwantchouhan44217@gmail.com>
2025-10-06 13:00:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import './App.css'
-import NavBar from './components/NavBar'
-import Form from './components/Form'
-import ListGroup from './components/ListGroup'
-import { FeedbackProvider } from './context/FeedbackContext'
-import RatingCount from './components/RatingCount'
+import "./App.css";
+import Navbar from "./components/NavBar";
+import Form from "./components/Form";
+import ListGroup from "./components/ListGroup";
+import { FeedbackProvider } from "./context/FeedbackContext";
+import RatingCount from "./components/RatingCount";
 
-
-
-const App=()=>{
-
+const App = () => {
   return (
     <FeedbackProvider>
-      <NavBar/>
-        <div className="p-12">
-          <h1 className='text-center text-2xl font-semibold text-gray-700'>Enter Your Feedback</h1>
-          <Form/>
-          <RatingCount/>
-          <ListGroup/>
-        </div>
+      <Navbar />
+      <div className="p-12">
+        <h1 className="text-center text-2xl font-semibold text-gray-700">
+          Enter Your Feedback
+        </h1>
+        <Form />
+        <RatingCount />
+        <ListGroup />
+      </div>
     </FeedbackProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
