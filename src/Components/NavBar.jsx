8feb2ccca6b6aef2d4bb03b0ff9b5c477273edfd
Balwant Chouhan <balwantchouhan44217@gmail.com>
2025-10-10 12:41:40 +0530

    first commit

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 const NavBar = () => {
   return (
-    <nav className='bg-[#6B3F69] py-2.5 px-12'>
-        <h1 className='text-white font-bold text-2xl'>Feedback App</h1>
-        </nav>
-  )
-}
+    <nav className="bg-[#6B3F69] py-2.5 px-12">
+      <h1 className="text-white font-bold text-2xl">Feedback App</h1>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
