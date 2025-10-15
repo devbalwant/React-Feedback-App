ba11bf6002c6373fa6a04455f7536d40479027f5
Balwant Chouhan <balwantchouhan44217@gmail.com>
2025-10-15 13:03:38 +0530

    SolveP

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import { FeedbackProvider } from "./context/FeedbackContext";
 import NavBar from "./Components/NavBar";
 import ListGroup from "./components/ListGroup";
 import Form from "./Components/Form";
 import RatingCount from "./components/RatingCount";
+import { FeedbackProvider } from "./context/FeedbackContext";
 
 const App = () => {
   return (
-    <FeedbackProvider>
+   <FeedbackProvider>
       <NavBar />
       <div className="p-12">
         <h1 className="text-center text-2xl font-semibold text-gray-700">
